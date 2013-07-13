1c9a59f4c4ad0e6979f3bf5c6c4b6447442d91ad
Alice Lara <hello@alicelara.ca>
2013-07-13 17:10:31 -0400

    writing tests to make sure the user visits the page and actually sees a content

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -3,5 +3,9 @@ require 'spec_helper'
 describe 'Visitor' do # who is doing the action
 	context 'visits the front page' do # action		
 		it{should visit root_path} # the test itself
+		it "displays content on page" do
+			visit root_path
+			page.should have_selector('table')
+		end
 	end
 end
