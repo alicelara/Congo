6947dd43907ff1d393d9e079706659c70ef46b04
Alice Lara <hello@alicelara.ca>
2013-07-13 20:26:34 -0400

    adding td to index.html.erb to pass the test

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -4,9 +4,10 @@ describe 'Visitor' do # who is doing the action
 	context 'visits the front page' do # action		
 		it{should visit root_path} # the test itself
 		it "displays content on page" do
-			(1..3).each do |x|
-				product = FactoryGirl.create(:product)
+			(1..2).each do |x|
+			FactoryGirl.create(:product)
 			end
+			product = FactoryGirl.create(:product)
 			visit root_path
 			page.should have_selector('table')
 			page.all('tr').should have(3).items
