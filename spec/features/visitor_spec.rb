ad4ab1788f41d925bb9b0d63c986927e6b4637c2
Alice Lara <hello@alicelara.ca>
2013-07-13 20:14:04 -0400

    including new tests to check products attributes

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -5,12 +5,15 @@ describe 'Visitor' do # who is doing the action
 		it{should visit root_path} # the test itself
 		it "displays content on page" do
 			(1..3).each do |x|
-				FactoryGirl.create(:product)
+				product = FactoryGirl.create(:product)
 			end
 			visit root_path
 			page.should have_selector('table')
 			page.all('tr').should have(3).items
 			page.all('td').should have(9).items
+			page.should have_content(product.name)
+			page.should have_content(product.description)
+			page.should have_content(product.price)
 		end
 	end
 end
