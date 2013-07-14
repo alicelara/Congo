a5e2f54690d006706b7512d10e4695b7cced7604
Alice Lara <hello@alicelara.ca>
2013-07-13 20:01:17 -0400

    passed the 'display content on page' test

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -4,6 +4,9 @@ describe 'Visitor' do # who is doing the action
 	context 'visits the front page' do # action		
 		it{should visit root_path} # the test itself
 		it "displays content on page" do
+			(1..3).each do |x|
+				FactoryGirl.create(:product)
+			end
 			visit root_path
 			page.should have_selector('table')
 			page.all('tr').should have(3).items
